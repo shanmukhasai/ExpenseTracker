3f38efe785c04493bdabed4d99e213dc5110d95d
arunj18 <arunjoh@gmail.com>
2017-06-29 07:55:29 +0530

    Added Accordion feature to sidebar and ability to remove hamburger icon in case of no login (emulated)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,23 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  public loginVerified=true;
   public enableSide=false;
   public mainMargin='0%';
   public sidebarWidth='0%';
+  public showAcc=false;
+  public buttonType='&#9660;';
   constructor() { }
 
   ngOnInit() {
   }
-  public w3_open(){
-    this.enableSide=true;
-    this.mainMargin='25%';
-    this.sidebarWidth='25%';
+  public toggleAcc(){
+    if(this.showAcc){
+      this.buttonType='&#9660;'
+    }
+    else{
+      this.buttonType='&#9650;'
+    }
+    this.showAcc=!(this.showAcc);
   }
-  public w3_close(){
-    this.enableSide=false;
-    this.mainMargin='0%';
-    this.sidebarWidth='0%';
+  public w3_open(){
+    if(this.enableSide){
+      this.mainMargin='0%';
+      this.sidebarWidth='0%';
+    }
+    else{
+      this.mainMargin='25%';
+      this.sidebarWidth='25%';
+    }
+    this.enableSide=!(this.enableSide);
+  }
+  public hide(){
+    this.loginVerified=!this.loginVerified;
   }
 
 }
